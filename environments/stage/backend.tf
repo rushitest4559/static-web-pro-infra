9a9bd76a8e1cfff4a712c670df244af364137388
rushi <rushitest4559@gmail.com>
2026-01-14 19:18:58 +0530

    first commit

---
+++ environments/stage/backend.tf
@@ -0,0 +1,29 @@
+terraform {
+
+    required_version = "~> 1.14.0"
+
+    backend "azurerm" {
+        resource_group_name = "rg-state-files"
+        storage_account_name = "staticwebrushi"
+        container_name = "tfstate"
+        key = "stage.terraform.tfstate"
+        use_oidc = true
+    }
+
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "~> 3.90.0"
+        }
+    }
+
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+        prevent_deletion_if_contains_resources = true
+    }
+  }
+  use_oidc = true
+}
