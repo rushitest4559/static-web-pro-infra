153a22e0339a5605934eb58457e7673e73479498
rushi <rushitest4559@gmail.com>
2026-01-15 13:05:13 +0530

    provider versions in child modules removed

--- modules/storage-account/main.tf
+++ modules/storage-account/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.90.0"
-    }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = ">=2.47.0"
-    }
-  }
-}
-
 
 data "azuread_application" "github_oidc_app" {
   display_name = "static-web-app"
