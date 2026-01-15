a91c8d8029a99a04752e952e448e7ae979651148
rushi <rushitest4559@gmail.com>
2026-01-15 13:22:47 +0530

    storage account name fixed

--- modules/storage-account/main.tf
+++ modules/storage-account/main.tf
@@ -4,7 +4,7 @@ data "azuread_application" "github_oidc_app" {
 }
 
 resource "azurerm_storage_account" "sa" {
-    name = "rushistaticwebsitesa${var.env}"
+    name = "rushistaticsitesa${var.env}"
     account_replication_type = "LRS"
     account_tier = "Standard"
     resource_group_name = var.rg_name
